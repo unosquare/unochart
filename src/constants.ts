0d6f95854f039344d0d924518e43d77adb70f621
svsm03 <svsm03@hotmail.com>
2024-07-16 12:27:08 -0600

    Agregado constants.ts y BarChart/utils.ts

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_WIDTH = 400;
+export const DEFAULT_HEIGHT = 300;
+export const DEFAULT_MARGIN = 5;
+export const DEFAULT_BAR_CATEGORY_GAP = '10%';
+export const DEFAULT_BAR_GAP = 4;
+export const DEFAULT_LAYOUT: 'horizontal' | 'vertical' = 'horizontal';
+
+export type ChartData = Array<{ name: string; [key: string]: any }>;
