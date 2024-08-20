7525b7f05095302d03e7a84eb8b9fbcd1151186d
svsm03 <svsm03@hotmail.com>
2024-08-20 12:25:07 -0600

    Duplicated code fix

--- samples/pages/RangedBarChart.tsx
+++ samples/pages/RangedBarChart.tsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from '../../src';
-import BarChartControls from '../utils/BarChartControls';
+import BarChartWrapper from '../utils/BarChartWrapper';
+import { Bar } from '../../src';
+
 
 const RangedBarChart = () => {
-    const [width, setWidth] = useState(600);
-    const [height, setHeight] = useState(400);
-    const [barCategoryGap, setBarCategoryGap] = useState('10%');
-    const [barGap, setBarGap] = useState(4);
-    const [layout, setLayout] = useState<'horizontal' | 'vertical'>('horizontal');
-    const [margin, setMargin] = useState({ top: 5, right: 5, bottom: 5, left: 5 });
-    const [showXAxis, setShowXAxis] = useState(true);
-    const [showYAxis, setShowYAxis] = useState(true);
-    const [showCartesianGrid, setShowCartesianGrid] = useState(true);
-    const [showTooltip, setShowTooltip] = useState(true);
-    const [showLegend, setShowLegend] = useState(true);
-
     const data = [
         { name: '05-01', temperature: [-6, 12] },
         { name: '05-02', temperature: [6, 12] },
@@ -29,48 +18,13 @@ const RangedBarChart = () => {
 
     return (
         <div>
-            <BarChartControls
-                width={width}
-                setWidth={setWidth}
-                height={height}
-                setHeight={setHeight}
-                barCategoryGap={barCategoryGap}
-                setBarCategoryGap={setBarCategoryGap}
-                barGap={barGap}
-                setBarGap={setBarGap}
-                layout={layout}
-                setLayout={setLayout}
-                margin={margin}
-                setMargin={setMargin}
-                showXAxis={showXAxis}
-                setShowXAxis={setShowXAxis}
-                showYAxis={showYAxis}
-                setShowYAxis={setShowYAxis}
-                showCartesianGrid={showCartesianGrid}
-                setShowCartesianGrid={setShowCartesianGrid}
-                showTooltip={showTooltip}
-                setShowTooltip={setShowTooltip}
-                showLegend={showLegend}
-                setShowLegend={setShowLegend}
-            />
-            <BarChart
-                data={data}
-                width={width}
-                height={height}
-                margin={margin}
-                barCategoryGap={barCategoryGap}
-                barGap={barGap}
-                layout={layout}
-            >
-                {showCartesianGrid && <CartesianGrid strokeDasharray="3 3" />}
-                {showXAxis && <XAxis dataKey="day" />}
-                {showYAxis && <YAxis />}
-                {showTooltip && <Tooltip />}
-                {showLegend && <Legend />}
-                <Bar dataKey="temperature" fill="#8884d8" />
-            </BarChart>
-        </div>
+      <BarChartWrapper data={data}>
+        <Bar dataKey="temperature" fill="#8884d8" />
+      </BarChartWrapper>
+    </div>
     );
 };
+
+
 
 export default RangedBarChart;
